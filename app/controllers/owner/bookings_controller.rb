9c7380b937f7da35fbbd6a2b4e9fd7ce35e13900
Charles de La Cochetiere <charles.delacochetiere@gmail.com>
2017-03-17 11:32:32 +0100

    Began changes on the pending requests

--- app/controllers/owner/bookings_controller.rb
+++ app/controllers/owner/bookings_controller.rb
@@ -1,6 +1,6 @@
 class Owner::BookingsController < ApplicationController
 
   def index
-    @booked_hamacs_owner = current_user.bookings_as_owner
+    @pending_requests_for_owner = current_user.bookings_as_owner
   end
 end
